3f670aeef706f8c1683562e1e558e6da480fd357
Lab2-15 <Lab2-15@LAB2-15>
2025-04-03 18:27:24 -0600

    Agregar archivos de proyecto.

---
+++ loggin/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loggin
+{
+    public partial class Form1 : Form
+    {
+        Usuario casa = new Usuario();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string user = txbUsuario.Text;
+            string Password = txbContraseña.Text;
+
+            var lista = casa.ObtenerUsuario();
+            var valida = lista.FirstOrDefault(u => u.NombreUsuario == user && u.Contrasenia == Password );
+            if (valida != null)
+            {
+                this.Hide();
+                Inicio inicio = new Inicio();
+                inicio.Show();
+            }
+            else
+            {
+                MessageBox.Show("ERROR en algo");
+            }
+        }
+    }
+}
---
+++ loggin/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loggin
+{
+    internal class Usuario
+    {
+        private static List<Usuario> usuarios = new List<Usuario>
+        {
+            new Usuario{NombreUsuario = "Admin", Contrasenia = "1234"},
+            new Usuario{NombreUsuario = "Juan", Contrasenia = "3333"},
+            new Usuario{NombreUsuario = "Adrian", Contrasenia = "2548"}
+        };
+        public Usuario(string nombreUsuario, string contrasenia)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasenia = contrasenia;
+        }
+        public List<Usuario> ObtenerUsuario()
+        { 
+            return usuarios; 
+        }
+        public Usuario() { }
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get;set; }
+    }
+}
